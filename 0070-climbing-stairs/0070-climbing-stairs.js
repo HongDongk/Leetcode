c0ce7e2d62e4601ec105fe10f6a94e1fea2593bc
CoderDuck <64244383+HongDongk@users.noreply.github.com>
2025-02-09 21:55:26 +0900

    Time: 8 ms (2.85%), Space: 49.5 MB (8.52%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let dp = new Array(n+1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for(let i =3; i<=n; i++){
+        dp[i] = dp[i-2] + dp[i-1];
+    }
+
+    console.log(dp);
+
+    return dp[n];
+};
