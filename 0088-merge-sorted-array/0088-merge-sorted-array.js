3242a6838e01fda990cf7fdb3b7921f77e5ee1b2
CoderDuck <64244383+HongDongk@users.noreply.github.com>
2025-04-05 23:16:20 +0900

    Time: 1 ms (15.07%), Space: 54.4 MB (51.6%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let answer = [...nums1.slice(0,m), ...nums2].sort((a,b) => a-b);
+
+    for(let i =0; i<nums1.length; i++){
+        nums1[i] = answer[i];
+    }
+};
