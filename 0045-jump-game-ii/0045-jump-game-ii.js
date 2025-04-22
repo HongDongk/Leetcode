7ed25c7db6fd21d489f3eb80f1a232b6386ebe84
CoderDuck <64244383+HongDongk@users.noreply.github.com>
2025-04-22 10:14:13 +0900

    Time: 0 ms (100%), Space: 54.5 MB (90.23%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -4,15 +4,15 @@
  */
 var jump = function(nums) {
     let answer = 0;
-    let end = 0;
-    let farthest = 0;
+    let maxx = 0;
+    let currentmax = 0;
 
     for(let i =0; i<nums.length-1; i++){
-        farthest = Math.max(nums[i] + i, farthest);
-        if(i === end){
+        maxx = Math.max(maxx, i+nums[i]);
+        if(i === currentmax){
             answer++;
-            end = farthest;
-        } 
+            currentmax = maxx;
+        }
     }
 
     return answer;
