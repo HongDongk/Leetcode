3015b65dc9f55f7742bffd9b1868fe7bb77c524d
CoderDuck <64244383+HongDongk@users.noreply.github.com>
2025-06-03 21:45:13 +0900

    Time: 0 ms (100%), Space: 53.7 MB (58.53%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -5,21 +5,20 @@
  */
 var searchInsert = function(nums, target) {
     let answer;
-    let temp;
     let start = 0;
     let end = nums.length-1;
 
-    while(start <= end){
-        let mid = parseInt((start+end)/2);
-        if(nums[mid] === target){
-            answer = mid
-            break;
-        }else if(nums[mid] > target){
-            end = mid - 1;
+    while(start<=end){
+        let mid = Math.floor((start+end) / 2);
+
+        if(nums[mid] === target) return answer = mid;
+
+        if(nums[mid] < target){
+            start = mid+1;
         }else{
-            start = mid + 1;
+            end = mid-1;
         }
     }
 
-    return start;
+    return answer ? answer : start;
 };
