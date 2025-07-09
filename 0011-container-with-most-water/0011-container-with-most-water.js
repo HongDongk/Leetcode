2199e4a8f5ad81b4044c79a1bb25cf5256e9e4da
CoderDuck <64244383+HongDongk@users.noreply.github.com>
2025-07-09 17:42:53 +0900

    Time: 3 ms (53.52%), Space: 63.6 MB (16.14%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,14 +3,16 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let answer = 0;
     let start = 0;
-    let end = height.length-1;
+    let end = height.length - 1;
+    let answer = 0;
 
-    while(start<end){
-        answer = Math.max(answer, (end-start) * Math.min(height[start], height[end]));
+    while(start < end){
+        let width = end - start;
+        let h = Math.min(height[start], height[end]);
+        answer = Math.max(width*h, answer);
         
-        if(height[start] < height[end]){
+        if( height[start] < height[end]){
             start++;
         }else{
             end--;
