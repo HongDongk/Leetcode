7bf89e4ffa1c4f4b83927b3a561582daa77256ec
CoderDuck <64244383+HongDongk@users.noreply.github.com>
2025-07-14 11:21:08 +0900

    Time: 3 ms (68.11%), Space: 58.9 MB (29.41%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -4,18 +4,19 @@
  * @return {number}
  */
 var minSubArrayLen = function(target, nums) {
-    let start = 0;
-    let sum = 0;
     let answer = Infinity;
+    let left = 0;
+    let sum = 0;
 
-    for(let i =0; i<nums.length; i++){
+    for(let i = 0; i<nums.length; i++){
         sum += nums[i];
+
         while(sum >= target){
-            answer = Math.min(answer, i-start+1)
-            sum -= nums[start];
-            start++;
+            answer = Math.min(answer, i-left+1);
+            sum -= nums[left];
+            left++;
         }
     }
 
-    return answer === Infinity ? 0 : answer;
+    return answer === Infinity ? 0 : answer
 };
