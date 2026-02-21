8f5cde0c9170642af5457dfb7a61a5bf5564bd5b
CoderDuck <64244383+HongDongk@users.noreply.github.com>
2026-02-21 15:12:33 +0900

    Time: 1 ms (74.13%), Space: 53.9 MB (87.03%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -4,15 +4,10 @@
  */
 var maxProfit = function(prices) {
     let answer = 0;
-    let now = prices[0];
 
-    for(let i =1; i<prices.length; i++){
-        console.log(now);
-        if(now>prices[i]){
-            now = prices[i];
-        }else{
-            answer += prices[i] - now;
-            now = prices[i];
+    for(let i=1; i<prices.length; i++){
+        if(prices[i] > prices[i-1]){
+            answer += (prices[i] - prices[i-1]);
         }
     }
 
